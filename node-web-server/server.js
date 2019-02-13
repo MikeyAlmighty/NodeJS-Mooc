c0a4da18ec8798a6c99a87e8cd768c7db5a52828
Michael <mleibbrandt@fullfacing.com>
2019-02-13 14:50:12 +0200

    Added Heroku

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 
 app.set("view engine", "hbs");
+
+app.use((req, resp, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) console.log("Unable to append to server log.");
+  });
+  console.log(log);
+  next();
+});
+
+// app.use((req, resp, next) => {
+//   resp.render("maintenance.hbs");
+// });
+
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getCurrentYear", () => {
@@ -37,6 +55,6 @@ app.get("/bad", (req, resp) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server listeing on port 3000");
+app.listen(port, () => {
+  console.log(`Server listeing on port ${port}`);
 });
